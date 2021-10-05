8aae3d6c53e4c231548fae0176ed26b25025a9ca
thre-huishou <2721061625@qq.com>
2021-10-05 13:57:54 +0800

    day1

---
+++ Project1/Project1/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//设计一个小程序判断当前电脑的字节序
+int main()
+{
+	//int num=0x11223344;
+	//char *pc = (char *)&num;
+	//if (*pc == 0x11)
+	//{
+	//	printf("大端");
+	//}
+	//else
+	//{
+	//	printf("小端");
+	//}
+	//指针类型的意义
+	//int num = 0x11223344;
+	////int *p = &num;
+	//char *p = (char *)&num;
+	//*p = 0;
+	//printf("%d", num);
+	//整形提升：有符号补符号位、无符号位补0
+	//char a = 128;
+	//signed char b = -1;
+	//unsigned char c = -1;
+	//printf("a=%u", a);
+
+	//float
+	//int num = 9;
+	//float* p = (float*)&num;
+	//printf("%d\n", num);
+	//printf("%f\n", *p);
+
+	//*p = 9.0;
+	//printf("%d\n", num);
+	//printf("%f\n", *p);
+	 
+	return 0;
+}
